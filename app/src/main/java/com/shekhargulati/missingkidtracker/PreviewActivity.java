69fc489dcb58d89f589fee1c153a1f0cc6bbac64
Shekhar Gulati <shekhargulati84@gmail.com>
2016-06-26 17:19:09 +0530

    Added share button

--- app/src/main/java/com/shekhargulati/missingkidtracker/PreviewActivity.java
+++ app/src/main/java/com/shekhargulati/missingkidtracker/PreviewActivity.java
@@ -1,9 +1,15 @@
 package com.shekhargulati.missingkidtracker;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -17,6 +23,8 @@ public class PreviewActivity extends AppCompatActivity {
 
     public static final String CAPTURED_PHOTO_PATH = "capturedPhotoPath";
     private String capturedPhotoPath;
+    private ShareActionProvider shareActionProvider;
+    private Intent shareIntent = new Intent();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,26 @@ public class PreviewActivity extends AppCompatActivity {
         Intent intent = getIntent();
         this.capturedPhotoPath = intent.getStringExtra(CAPTURED_PHOTO_PATH);
         setPic();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(new File(capturedPhotoPath)));
+        shareIntent.setType("image/jpeg");
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.preview_menu, menu);
+        MenuItem shareMenuItem = menu.findItem(R.id.menu_share_photo);
+        shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+        setShareIntent(shareIntent);
+        return true;
+    }
+
+    private void setShareIntent(Intent shareIntent) {
+        if (shareActionProvider != null) {
+            shareActionProvider.setShareIntent(shareIntent);
+        }
     }
 
     private void setPic() {
